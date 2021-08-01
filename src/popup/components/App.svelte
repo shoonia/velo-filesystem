71c3d69ac4716d7353304e4699f0487fee7e7f12
shoonia <alexanderz@wix.com>
2021-08-01 19:00:38 +0300

    Add Button component

--- src/popup/components/App.svelte
+++ src/popup/components/App.svelte
@@ -1,1 +1,19 @@
 <h1>Hello</h1>
+<style>
+  :global(*),
+  :global(*::before),
+  :global(*::after) {
+    box-sizing: border-box;
+  }
+
+  :global(html),
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(body) {
+    color: #162d3d;
+    background-color: #f0f4f7;
+  }
+</style>
---
+++ src/popup/components/Button.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let onClick: EventHandlerNonNull;
+</script>
+
+<button
+  type="button"
+  on:click={onClick}
+>
+  <slot></slot>
+</button>
+
+<style>
+  button {
+    transition: all 0.2s ease, visibility 0s;
+    border-radius: 100px;
+    background-color: #3637f5;
+    color: #fff;
+    font-size: 14px;
+    cursor: pointer;
+    border: none;
+    display: block;
+    min-width: 10px;
+    min-height: 10px;
+    padding: 8px 20px;
+    user-select: none;
+    white-space: normal;
+  }
+
+  button:hover {
+    background: #2224e2;
+  }
+</style>
