e66de408fd6dd3c36ad4ffe877be15eb5d370db0
shoonia <alexanderz@wix.com>
2021-09-28 22:50:32 +0300

    Use sync method

--- zip.js
+++ zip.js
@@ -1,8 +1,8 @@
 import { createRequire } from 'module';
-import { zip } from 'cross-zip';
+import { zipSync } from 'cross-zip';
 
 const require = createRequire(import.meta.url);
 
 const pkg = require('./package.json');
 
-await zip('./build', `./${pkg.name}@${pkg.version}.zip`);
+zipSync('./build', `./${pkg.name}@${pkg.version}.zip`);
