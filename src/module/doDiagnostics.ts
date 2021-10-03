51cbb326cde4c843e834adf2d5163904252d629f
shoonia <alexanderz@wix.com>
2021-10-03 12:51:03 +0300

    Add doDiagnostics script

---
+++ src/module/doDiagnostics.ts
@@ -0,0 +1,42 @@
+import type { languages } from 'monaco-editor';
+import { getJsModels, getJsWorker } from './textModel';
+
+interface IDiagnosticsResult {
+  readonly path: string;
+  readonly syntax: languages.typescript.Diagnostic[];
+  readonly semantic: languages.typescript.Diagnostic[];
+}
+
+interface IDoDiagnostics {
+  (): Promise<IDiagnosticsResult[]>
+}
+
+export const doDiagnostics: IDoDiagnostics = async () => {
+  const jsModels = getJsModels();
+  const jsWorker = await getJsWorker();
+
+  if (jsModels.length < 0 || typeof jsWorker !== 'function') {
+    return [];
+  }
+
+  const tasks = jsModels.map(async (model) => {
+    const modelWorker = await jsWorker(model.uri);
+    const fileName = model.uri.toString();
+
+    const [
+      syntax,
+      semantic,
+    ] = await Promise.all([
+      modelWorker.getSyntacticDiagnostics(fileName),
+      modelWorker.getSemanticDiagnostics(fileName),
+    ]);
+
+    return {
+      path: model.uri.path,
+      syntax,
+      semantic,
+    };
+  });
+
+  return Promise.all(tasks);
+};
