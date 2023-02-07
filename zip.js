474f2e346a9a200d0313296010792a8e8fc87742
shoonia <alexanderz@wix.com>
2023-02-07 20:54:36 +0200

    Update Node.js version

--- zip.js
+++ zip.js
@@ -1,8 +1,6 @@
 import { createRequire } from 'node:module';
 import { zipSync } from 'cross-zip';
 
-const require = createRequire(import.meta.url);
-
-const pkg = require('./package.json');
+const pkg = createRequire(import.meta.url)('./package.json');
 
 zipSync('./build', `./${pkg.name}@${pkg.version}.zip`);
