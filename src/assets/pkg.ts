e6503265884d9e549b75b73a4a3343533b406af2
shoonia <alexanderz@wix.com>
2023-02-25 22:16:38 +0200

    Add metadata

--- src/assets/pkg.ts
+++ src/assets/pkg.ts
@@ -3,5 +3,11 @@ import { version, discussions } from '../../package.json';
 export { version, discussions };
 
 export const getMetaFileValue = (): string => {
-  return JSON.stringify({ version }, null, 2);
+  return JSON.stringify({
+    version,
+    editor: {
+      dateCreated: new Date().toISOString(),
+      url: location.href,
+    },
+  }, null, 2);
 };
