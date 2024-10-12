3135ce84170b72a2eeae33fdc068ea75aa0c139e
shoonia <alexanderz@wix.com>
2024-10-13 00:41:19 +0300

    Migrate ESLint to v9

---
+++ eslint.config.js
@@ -0,0 +1,66 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+
+export default ts.config(
+  {
+    ignores: [
+      'node_modules/*',
+      'build',
+      '!src/*.js',
+    ],
+  },
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      globals: {
+        process: 'readonly',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-use-before-define': 'error',
+      'no-else-return': 'error',
+      'no-trailing-spaces': 'error',
+      'no-use-before-define': 'off',
+      'indent': [
+        'error',
+        2,
+        {
+          'SwitchCase': 1,
+        },
+      ],
+      'semi': [
+        'error',
+        'always',
+      ],
+      'space-before-function-paren': [
+        'error',
+        {
+          'anonymous': 'always',
+          'named': 'never',
+          'asyncArrow': 'always',
+        },
+      ],
+      'comma-dangle': [
+        'error',
+        'always-multiline',
+      ],
+      'no-unused-vars': 'off',
+      'quotes': [
+        'error',
+        'single',
+      ],
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          'max': 1,
+          'maxBOF': 0,
+          'maxEOF': 0,
+        },
+      ],
+    },
+  },
+);
